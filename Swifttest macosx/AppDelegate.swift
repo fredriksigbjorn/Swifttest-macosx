6d0e5c7097390ea40b5fc78fd465dacc64e2d832
Fredrik Sigbjörn <thyrr@sigges-imac.bredbandsbolaget.se>
2017-01-15 16:22:14 +0100

    Initial Commit

---
+++ Swifttest macosx/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Swifttest macosx
+//
+//  Created by Fredrik Sigbjörn on 2017-01-15.
+//  Copyright © 2017 Fredrik Sigbjörn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
